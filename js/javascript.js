9696a3c60dab0e345d333d871eeae6a2173db498
justin h <joosteen96@gmail.com>
2022-11-03 14:00:27 -0700

    Add winner announcement after reaching 5 points

--- js/javascript.js
+++ js/javascript.js
@@ -2,25 +2,40 @@ const buttons = document.querySelectorAll('button');
 const container = document.querySelector('#container');
 const resultMsg = document.createElement('div');
 const scoreBoard = document.createElement('div');
+const winnerMessage = document.createElement('div');
 let playerHand = '';
 let playerScore = 0;
 let computerScore = 0;
 let tieScore = 0;
 
-    buttons.forEach((button) => {
-        button.addEventListener('click', () => {
-            playerHand = button.id;
-            gameRound(playerHand, getComputerChoice());
-            scoreOutput(playerScore, computerScore, tieScore);
-        });
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerHand = button.id;
+        gameRound(playerHand, getComputerChoice());
+        scoreOutput(playerScore, computerScore, tieScore);
+        checkWinner(playerScore, computerScore);
     });
+});
 
+
+//outputs current score to DOM
 function scoreOutput (playScore, compScore, tScore) {
     scoreBoard.classList.add('scoreBoard');
     scoreBoard.textContent = `You: ${playScore} | Computer: ${compScore} | Tied: ${tScore}`;
     container.appendChild(scoreBoard);
 }
 
+function checkWinner (playScore, compScore) {
+    if (playScore === 5) {
+        winnerMessage.classList.add('winnerMessage');
+        winnerMessage.textContent = "You won!";
+        container.appendChild(winnerMessage);
+    } else if (compScore === 5) {
+        winnerMessage.classList.add('winnerMessage');
+        winnerMessage.textContent = "Computer won!";
+        container.appendChild(winnerMessage);
+    }
+}
 
 
 function getComputerChoice() {
